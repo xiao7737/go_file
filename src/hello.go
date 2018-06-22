f6c9fa090840544fa78347f225513cec203d4c0a
xiao <773708486@qq.com>
2018-06-22 11:53:57 +0800

    second commit

--- src/hello.go
+++ src/hello.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	hello "fmt"
+)
 
-func main()  {
-	fmt.Println("hello 世界你好")
+func main() {
+
+	hello.Println("The first is mine")
+
 }
+
+//byte = uint8  0-255     int8 :-128-127
+//rune = int32
+//float32/float64   长度4字节，小数位7位/8字节，小数位15位
+//零值不等于空值，是申明后的默认值。默认值为0，bool为flase，string为空字符串
