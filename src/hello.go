a45ceceff31b56746f584732ce35c96c953fe8cf
xiao <773708486@qq.com>
2018-06-23 08:28:29 +0800

    test

--- src/hello.go
+++ src/hello.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	hello "fmt"
+	 "fmt"
 )
 
 func main() {
-
-	hello.Println("The first is mine！")
-
+	fmt.Println("The first is mine！")
 }
+
+
+
 
 //byte = uint8  0-255     int8 :-128-127
 //rune = int32
 //float32/float64   长度4字节，小数位7位/8字节，小数位15位
-//零值不等于空值，是申明后的默认值。默认值为0，bool为flase，string为空字符串
+//零值不等于空值，是申明后的默认值。默认值为0，bool为false，string为空字符串
