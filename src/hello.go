94874e417cc54262de7740fa0aab6956a541ff16
xiao <773708486@qq.com>
2018-06-23 17:33:33 +0800

    heiheihei

--- src/hello.go
+++ src/hello.go
@@ -9,13 +9,15 @@ func main() {
 }
 
 
-<<<<<<< HEAD
-=======
-	hello.Println("The first is mine！！")
->>>>>>> 291d1bc9220018429e827b599104e364370bfbd3
+
 
 
 //byte = uint8  0-255     int8 :-128-127
 //rune = int32
 //float32/float64   长度4字节，小数位7位/8字节，小数位15位
 //零值不等于空值，是申明后的默认值。默认值为0，bool为false，string为空字符串
+//int 和 float可以相互转换，bool和int不能
+//string 转换 int   需要用到strconv包，strconv.Atoi
+//int 转换 string   strconv.Itoa
+//常量表达式中只能出现常量，常量组中，如果常量没有赋值，则使用上一行的值
+ 
---
+++ src/test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+const c  = 2
+
+func main()  {
+	a:=65
+	b:=strconv.Itoa(a)
+	fmt.Println(b)
+	fmt.Println(c)
+}
