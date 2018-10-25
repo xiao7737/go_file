49203517673a6f804db048f50f10a2b6970abd26
xiao7737 <773708486@qq.com>
2018-10-25 21:19:49 +0800

    slice学习

--- src/Slice.go
+++ src/Slice.go
@@ -3,9 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	s1 := []int{1, 2, 3, 4, 5, 6}
+	/*s1 := []int{1, 2, 3, 4, 5, 6}
 	s2 := []int{7, 8}
 	copy(s1, s2) //将切片2拷贝到切片1,切片大小以目标切片为准
 
-	fmt.Println(s1) //783456，如将s1拷贝到s2.输出12
+	fmt.Println(s1) //783456，如将s1拷贝到s2.输出12*/
+
+	p := []int{1, 2, 3, 4, 5}
+	fmt.Println("p==", p)
+	pLen := len(p)
+	for i := 0; i < pLen; i++ {
+		fmt.Printf("p[%d]==%d\n", i, p[i])
+	}
+
 }
