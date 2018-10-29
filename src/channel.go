c52682a67a201073083b872cd954330c6d49b625
xiao7737 <773708486@qq.com>
2018-10-29 18:25:52 +0800

    channel

--- src/channel.go
+++ src/channel.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func sum(s []int, c chan int) {
+/*func sum(s []int, c chan int) {
 	sum := 0
 	for _, v := range s {
 		sum += v
@@ -16,4 +16,28 @@ func main() {
 	go sum(a[len(a)/2:], c) //送入c的值为7
 	x, y := <-c, <-c        //从管道中获取
 	fmt.Println(x, y)       //7，3
+}*/
+
+//用channel select实现斐波纳契数据
+func fibonacci(c, quit chan int) {
+	x, y := 0, 1
+	for {
+		select {
+		case c <- x:
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("quit")
+		}
+	}
+}
+func main() {
+	c := make(chan int)
+	quit := make(chan int)
+	go func() {
+		for i := 1; i <= 5; i++ {
+			fmt.Println(<-c)
+		}
+		quit <- 0
+	}()
+	fibonacci(c, quit)
 }
