e9e899811ad0f34f2e4d36aafa0317a9926fcff8
xiao <773708486@qq.com>
2019-01-09 22:26:03 +0800

    functin as a type

--- src/Learn.go
+++ src/Learn.go
@@ -2,9 +2,36 @@ package main
 
 import "fmt"
 
+//函数也是一种类型，申明一个参数类型为函数，就可以将函数作为另一个函数的参数参入
+//增强程序的灵活性，用来通用接口
+type testInt func(int) bool //申明一个函数类型
+
+func isOdd(integer int) bool {
+	if integer%2 == 0 {
+		return false
+	}
+	return true
+}
+func isEven(integer int) bool {
+	if integer%2 == 0 {
+		return true
+	}
+	return false
+}
+func filter(slice []int, f testInt) []int { //参数类型为函数
+	var result []int
+	for _, value := range slice {
+		if f(value) {
+			result = append(result, value)
+		}
+	}
+	return result
+}
 func main() {
-	var a [2]string
-	a[0] = "hello"
-	a[1] = "world"
-	fmt.Println(a)
+	slice := []int{1, 2, 3, 4, 5, 6, 7}
+	fmt.Println("slice =", slice)
+	odd := filter(slice, isOdd)   //函数当作值来传递
+	even := filter(slice, isEven) //同上
+	fmt.Println("odd value of slice are:", odd)
+	fmt.Println("even value of slice are:", even)
 }
