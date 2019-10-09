97e42dab87bad49a8b8ed30160b63d47b0a42e39
xiao7737 <773708486@qq.com>
2019-10-09 15:10:08 +0800

    多值返回

--- src/swap.go
+++ src/swap.go
@@ -2,8 +2,10 @@ package main
 
 import "fmt"
 
-func swap(x, y string) (string, string) {
-	return y, x
+//多值返回
+func swap(x, y string) (a, b string) {
+	b, a = x, y
+	return
 }
 func main() {
 	fmt.Println(swap("hello", "go"))
