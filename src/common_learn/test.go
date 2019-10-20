ded47f53c98e8d54bf372d769d862ea79ad02aea
xiao7737 <773708486@qq.com>
2019-10-20 13:03:13 +0800

    map is not to be compare

--- src/common_learn/test.go
+++ src/common_learn/test.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	m1 := map[string]string{"one": "one"}
+	m2 := map[string]string{"one": "one"}
+	m1["one"] = "one"
+	m2["one"] = "one"
+	if m1["one"] == m2["one"] {
+		fmt.Println("m1 is equals to m2")
+	}
 }
