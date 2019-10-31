148631bb0923233ce22a5d611757860a569c34d2
xiao7737 <773708486@qq.com>
2019-10-31 13:23:31 +0800

    go的http包

--- src/common_learn/test.go
+++ src/common_learn/test.go
@@ -1,8 +1,15 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	var rwm sync.RWMutex
-	rwm.RLocker()
+	http.HandleFunc("/", httpHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+func httpHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello go http")
 }
