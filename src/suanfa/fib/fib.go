26c7d93c6c9e870ff35372afafebea8790cd154a
xiao7737 <773708486@qq.com>
2019-11-21 23:56:56 +0800

    找寻斐波那契数列的第n和数；宝石和石头问题

---
+++ src/suanfa/fib/fib.go
@@ -0,0 +1,10 @@
+package fib
+
+//返回斐波那契数列中指定的第n和数
+func fib(n int) int {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		x, y = y, x+y
+	}
+	return x
+}
---
+++ src/suanfa/numJewelsInStones/numJewelsInStones.go
@@ -0,0 +1,32 @@
+package numJewelsInStones
+
+//返回S中存在J的个数，宝石与石头问题
+//J a   S  abca   返回2
+
+//暴力解法
+func numJewelsInStones(J string, S string) int {
+	var i = 0
+	for j := 0; j < len(J); j++ {
+		for s := 0; s < len(S); s++ {
+			if J[j] == S[s] {
+				i++
+			}
+		}
+	}
+	return i
+}
+
+//利用map
+func numJewelsInStones2(J string, S string) int {
+	count := 0
+	jmap := make(map[rune]int)
+	for _, v := range J {
+		jmap[v] = 1
+	}
+	for _, v := range S {
+		if _, ok := jmap[v]; ok {
+			count++
+		}
+	}
+	return count
+}
