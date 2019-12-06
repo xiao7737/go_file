4c48e2a62ac698806ef1ed5c5bd51524b40c3fe5
xiao7737 <773708486@qq.com>
2019-12-06 17:07:19 +0800

    判断扩号是否合法，LeetCode20

---
+++ src/suanfa/isValid/isValid.go
@@ -0,0 +1,40 @@
+package isValid
+
+//判断一个包含扩号的字符串是否有效。空字符串为有效，扩号必须有序，{{(}}) 不符合，{{()}} 符合
+//思路：每将一个字符放入一个栈，就去和栈内的最后一个元素匹配，如果匹配，就从栈中删除这个元素，否则就入栈
+//扩号要有序，所以每次只需要和栈的最后一个元素进行对比就可以了
+
+func isValid(s string) bool {
+	if len(s) == 0 {
+		return true
+	}
+	if len(s)%2 != 0 {
+		return false
+	}
+
+	//匹配字典
+	m := map[string]string{
+		")": "(",
+		"]": "[",
+		"}": "{",
+	}
+	var stack []string
+	l := len(s)
+	for i := 0; i < l; i++ {
+		if len(stack) == 0 {
+			stack = append(stack, string(s[i]))
+		} else {
+			//进行匹配，匹配成功，删除栈的最后一个元素，否则，加入到栈中
+			if m[string(s[i])] == stack[len(stack)-1] {
+				stack = stack[:len(stack)-1]
+			} else {
+				stack = append(stack, string(s[i]))
+			}
+		}
+	}
+	if len(stack) != 0 {
+		return false
+	} else {
+		return true
+	}
+}
