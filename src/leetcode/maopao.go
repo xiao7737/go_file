fd5c19a34c434bf6c8c22bf53fe9cfca42f222a2
xiao7737 <773708486@qq.com>
2019-12-11 16:26:39 +0800

    冒泡两种方向

--- src/leetcode/maopao.go
+++ src/leetcode/maopao.go
@@ -2,14 +2,14 @@ package main
 
 import "fmt"
 
-//从大到小冒泡排序
+//从小到大冒泡排序
 func main() {
-	a := [...]int{1, 3, 4, 5, 7, 9}
+	a := [...]int{1, 3, 5, 4, 7, 9}
 	fmt.Println(a)
 	length := len(a)
 	for i := 0; i < length; i++ {
 		for j := i + 1; j < length; j++ {
-			if a[i] < a[j] {
+			if a[i] > a[j] { //从大到小，改为 <
 				temp := a[i]
 				a[i] = a[j]
 				a[j] = temp
