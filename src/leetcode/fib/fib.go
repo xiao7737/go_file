db4f758d6d4aba9df352eb4ac1a6547cf331c246
xiao7737 <773708486@qq.com>
2020-03-01 15:05:47 +0800

    优化斐波那契数列-使用内存缓存减少重复计算

--- src/leetcode/fib/fib.go
+++ src/leetcode/fib/fib.go
@@ -18,3 +18,20 @@ func fib1(n int) (res int) {
 	}
 	return
 }
+
+//利用计算机的内存缓存来优化，因为每次计算第n个数都要计算n的前两个数，可以将计算过的值存起来减少重复计算量
+var fibs [10]int //装计算好的数组
+
+func fib2(n int) (res int) {
+	if fibs[n] != 0 {
+		res = fibs[n]
+		return
+	}
+	if n < 2 {
+		res = 1
+	} else {
+		res = fib1(n-1) + fib1(n-2)
+	}
+	fibs[n] = res
+	return
+}
