da979fe632f51089859e7950ad955ccb15c20000
xiao7737 <773708486@qq.com>
2020-03-10 15:42:21 +0800

    用select来做超时处理

--- src/common_learn/channel.go
+++ src/common_learn/channel.go
@@ -1,8 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go1.13/src/time"
+)
 
-func sum(s []int, c chan int) {
+/*func sum(s []int, c chan int) {
 	sum := 0
 	for _, v := range s {
 		sum += v
@@ -16,10 +19,10 @@ func main() {
 	go sum(a[len(a)/2:], c) //送入c的值为7=3+4
 	x, y := <-c, <-c        //从管道中获取
 	fmt.Println(x, y)       //7，3
-}
+}*/
 
 //用channel select实现斐波纳契数据
-/*func fibonacci(c, quit chan int) {
+func fibonacci(c, quit chan int) {
 	x, y := 0, 1
 	for {
 		select {
@@ -27,6 +30,9 @@ func main() {
 			x, y = y, x+y
 		case <-quit:
 			fmt.Println("quit")
+		case <-time.After(time.Millisecond):
+			fmt.Println("time out")
+
 		}
 	}
 }
@@ -40,7 +46,7 @@ func main() {
 		quit <- 0
 	}()
 	fibonacci(c, quit)
-}*/
+}
 
 //channel源码解析
 //channel底层数据是hchan struct
