46dc2a6ed924e0114e25b8b34cbb47be0e49af9b
xiao7737 <773708486@qq.com>
2020-03-24 23:44:20 +0800

    二分：查找第一个出现给定值的元素，查找第一个大于给定值的元素

--- src/leetcode/search_algo/binary_search/binary_search.go
+++ src/leetcode/search_algo/binary_search/binary_search.go
@@ -1,16 +1,11 @@
 package main
 
 // 二分查找 logN，要求数组有序
-/*func main() {
-	var searchKey = 6
-	var sortedArr = []int{1, 3, 4, 6, 10, 13}
-	result := inarySearch(sortedArr, searchKey)
-	if result >= 0 {
-		fmt.Println("Find the index: ", result)
-	} else {
-		fmt.Println("Not find!")
-	}
-}*/
+// 可优化点：
+// 如果low和high特别大，两者之和可能溢出，考虑换成
+// mid = low+(high-low)/2
+// 更激进的写法，利用位运算代替除法
+// mid = low+((high-low)>>1)
 
 func BinarySearch(sortedArr []int, searchKey int) int {
 	var low = 0
@@ -28,7 +23,44 @@ func BinarySearch(sortedArr []int, searchKey int) int {
 	return -1
 }
 
-// 如果low和high特别大，两者之和可能溢出，考虑换成
-// mid = low+(high-low)/2
-// 更激进的写法，利用位运算代替除法
-// mid = low+((high-low)>>1)
+// 变种1：查找第一个searchKey出现的元素（数组有重复元素）
+func BinarySearch1(sortedArr []int, searchKey int) int {
+	var low = 0
+	var high = len(sortedArr) - 1
+	for low <= high {
+		var mid = low + ((high - low) >> 1)
+		if sortedArr[mid] > searchKey {
+			high = mid - 1
+		} else if sortedArr[mid] < searchKey {
+			low = mid + 1
+		} else {
+			// 如果mid为0，肯定是第一个了；如果该mid的前一个数不是search，那么该mid就是第一个出现的searchKey
+			if (mid == 0) || (sortedArr[mid-1] != searchKey) {
+				return mid
+			} else {
+				high = mid - 1
+			}
+		}
+	}
+	return -1
+}
+
+// 变种2：查找第一个大于searchKey的元素
+func BinarySearch2(sortedArr []int, searchKey int) int {
+	var low = 0
+	var high = len(sortedArr) - 1
+	for low <= high {
+		var mid = low + ((high - low) >> 1)
+		if sortedArr[mid] >= searchKey {
+			// mid的前一个小于searchKey，而当前mid大于searchKey
+			if (mid == 0) || (sortedArr[mid-1] < searchKey) {
+				return mid
+			} else {
+				high = mid - 1
+			}
+		} else {
+			low = mid + 1
+		}
+	}
+	return -1
+}
--- src/leetcode/search_algo/binary_search/binary_search_test.go
+++ src/leetcode/search_algo/binary_search/binary_search_test.go
@@ -25,3 +25,19 @@ func BenchmarkBinarySearch(b *testing.B) {
 		BinarySearch([]int{1, 3, 4, 6, 10, 13}, 6)
 	}
 }
+
+func TestBinarySearch1(t *testing.T) {
+	var tests = []struct {
+		sortedArr []int
+		searchKey int
+		want      int
+	}{
+		{[]int{1, 3, 4, 6, 6, 13}, 6, 3},
+		{[]int{1, 3, 4, 4, 4, 13}, 4, 2},
+	}
+	for _, test := range tests {
+		if got := BinarySearch1(test.sortedArr, test.searchKey); got != test.want {
+			t.Errorf("BinarySearch1(%v, %d) = %d", test.sortedArr, test.searchKey, got)
+		}
+	}
+}
