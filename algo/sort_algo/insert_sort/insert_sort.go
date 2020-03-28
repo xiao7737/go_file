cf7e567a08cf4a1a7a058f6a2dcaac26426f8d7c
xiao7737 <773708486@qq.com>
2020-03-28 19:25:42 +0800

    选择排序

--- algo/sort_algo/insert_sort/insert_sort.go
+++ algo/sort_algo/insert_sort/insert_sort.go
@@ -1,7 +1,7 @@
 package main
 
 // 插入排序 -- 尾插法  时间复杂度O(n*n)，空间O(1)
-// 思路：在未排序的数据中取出，插入到已经排序好的数组中
+// 思路：在未排序的数据中依次取出一个元素，和已经排序好的数组循环比较，交换位置
 // 可以参考源码sort的插入排序
 func InsertSort(arr []int) []int {
 	arrLen := len(arr)
@@ -11,11 +11,8 @@ func InsertSort(arr []int) []int {
 	for i := 1; i < arrLen; i++ {
 		tmp := arr[i] //未排序数据中取的数
 		for j := i - 1; j >= 0; j-- {
-			if tmp >= arr[j] {
-				//如果插入的元素tmp>=已经排序数组的最后一个元素,那么不做交换
-				break
-			} else {
-				//如果插入的元素比已经排序数组的最后一个元素要小，逐渐和已排序数组的元素进行交换，直到tmp>=排序数组的元素
+			if tmp < arr[j] {
+				//如果插入的元素比已经排序数组的最后一个元素要小，和已排序数组的元素进行交换，直到tmp>=排序数组的元素
 				arr[j], arr[j+1] = tmp, arr[j]
 			}
 		}
--- algo/sort_algo/select_sort/select_sort.go
+++ algo/sort_algo/select_sort/select_sort.go
@@ -24,3 +24,8 @@ func SelectSort(arr []int) []int {
 	}
 	return arr
 }
+
+//  3,1,4,2
+//	1,3,4,2
+//	1,2,4,3
+//	1,2,3,4
