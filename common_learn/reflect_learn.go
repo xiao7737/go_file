8ca9992b6df455ae9df02215a65bd37fab17003a
xiao7737 <773708486@qq.com>
2020-04-06 15:10:11 +0800

    反射

--- common_learn/reflect_learn.go
+++ common_learn/reflect_learn.go
@@ -7,8 +7,7 @@ import (
 
 // 利用反射调用函数
 // 太秀了==！
-
-func add(a, b int) int {
+/*func add(a, b int) int {
 	return a + b
 }
 func main() {
@@ -16,4 +15,24 @@ func main() {
 	paramList := []reflect.Value{reflect.ValueOf(10), reflect.ValueOf(20)} // 构造函数参数, 传入两个整型值
 	retList := funcValue.Call(paramList)                                   // 反射调用函数
 	fmt.Println(retList[0].Int())                                          // 获取第一个返回值, 取整数值
+}*/
+
+// 通过反射获取type和value
+func CheckType(v interface{}) {
+	t := reflect.TypeOf(v)
+	switch t.Kind() {
+	case reflect.Int:
+		fmt.Println("integer param")
+	case reflect.String:
+		fmt.Println("string param")
+	default:
+		fmt.Println("unknown param type")
+	}
+}
+func main() {
+	a := 1
+	CheckType(a)
+	fmt.Println(reflect.TypeOf(a))
+	fmt.Println(reflect.ValueOf(a))
+	fmt.Println(reflect.ValueOf(a).Type())
 }
