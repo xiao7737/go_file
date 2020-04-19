96b46917d534ed2f7f4684b63a0b4685af3d1013
xiao7737 <773708486@qq.com>
2020-04-19 15:33:13 +0800

    利用计算机缓存，来减少斐波那契数列的计算次数

--- algo/leetcode/fib/fib.go
+++ algo/leetcode/fib/fib.go
@@ -7,18 +7,16 @@ import (
 
 const LIM = 41
 
-var fibs [LIM]uint64 //装计算好的数组
+var fibs [LIM]int //装计算好的数组
 func main() {
-	/*var result uint64= 0  */
 	var result = 0
 	start := time.Now()
 	for i := 1; i < LIM; i++ {
 		result = fib(i)
 		fmt.Printf("数列第 %d 位: %d\n", i, result)
 	}
-
-	delta := time.Since(start)
-	fmt.Printf("程序的执行时间为: %s\n", delta)
+	run := time.Since(start)
+	fmt.Printf("程序的执行时间为: %s\n", run)
 }
 
 //返回斐波那契数列中指定的第n个数
@@ -32,7 +30,7 @@ func fib(n int) int {
 }
 
 //使用递归:返回斐波那契数列中指定的第n个数  1s
-func fib1(n int) (res uint64) {
+func fib1(n int) (res int) {
 	if n <= 2 {
 		res = 1
 	} else {
@@ -43,7 +41,7 @@ func fib1(n int) (res uint64) {
 
 //利用计算机的内存缓存来优化，因为每次计算第n个数都要计算n的前两个数，可以将计算过的值存起来减少重复计算量
 //对比fib1()，优势巨大，可以提升一个数量级  0.1s
-func fib2(n int) (res uint64) {
+func fib2(n int) (res int) {
 	if fibs[n] != 0 {
 		res = fibs[n]
 		return
