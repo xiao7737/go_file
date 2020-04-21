ddc83d903a52cf444e7630f42275689326441c39
xiao7737 <773708486@qq.com>
2020-04-21 17:06:25 +0800

    defer瞬间返回，不会等return再计算返回

--- common_learn/defer_2.go
+++ common_learn/defer_2.go
@@ -13,7 +13,7 @@ func main() {
 	fmt.Println("2")
 }
 
-// defer getFun()   //返回2，1，内部的3不会执行，defer会丢弃返回值
+// defer getFun()   //返回2，1，内部的3不会执行，defer会丢弃返回值，因为defer是瞬间计算，不会等return再计算
 // 返回1,2,3    解析：真正进入defer的是getFun的返回值，所以1执行后，3就进入的defer函数栈 ，然后执行2，最后执行defer栈的3
 // 有点意思！
 // 源码位置 runtime2.go  _defer
