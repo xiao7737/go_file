d19af19222cf9065ad48493824e6d6009db21695
xiao7737 <773708486@qq.com>
2020-04-26 19:38:04 +0800

    json_reflect_map.go

--- common_learn/json_reflect_map.go
+++ common_learn/json_reflect_map.go
@@ -17,9 +17,9 @@ func main() {
 	var m map[string]interface{}
 	_ = json.Unmarshal(b, &m)
 	for k, v := range m {
-		fmt.Printf("key:%v value:%v type of value:%T\n", k, v, v)
-		//此处的age变成了float64，小坑
-		//json中没有整形和浮点之分，全部转换为float64
-		//采用json.Decoder代替jsonUnmarshal解决问题
+		fmt.Printf("key:%v value:%v type of value:%T\n", k, v, v) //此处的age是float64
 	}
 }
+
+//json中没有整形和浮点之分，全部转换为float64
+//采用json.Decoder代替jsonUnmarshal解决问题
