64a53151a004512c022f9898431b2c62f63bd2ac
xiao7737 <773708486@qq.com>
2020-05-25 09:29:29 +0800

    interface的理解

--- common_learn/Interface.go
+++ common_learn/Interface.go
@@ -8,7 +8,7 @@ type Phone interface {
 type IPhone struct {
 }
 
-func (Phone *IPhone) call() {
+func (i *IPhone) call() {
 	fmt.Println("is iphone")
 }
 func main() {
@@ -22,4 +22,5 @@ func main() {
 在main函数里面定义了一个Phone接口类型变量，并分别为之赋值为IPhone。然后调用call()方法
 
 接口是非入侵式，不依赖接口的定义，采用 duck type  鸭子，像鸭子的样子就叫鸭子了 🦆
+接口设计优点：让接口和实现者真正解耦，降低项目的耦合度
 */
