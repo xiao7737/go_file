e77271ab890cb3ab80a04787264fb46aac0b1230
xiao7737 <773708486@qq.com>
2020-06-19 10:13:56 +0800

    对指针类型的range改变值

--- common_learn/range.go
+++ common_learn/range.go
@@ -13,4 +13,10 @@ func main() {
 		data[k] = 10 * v
 	}
 	fmt.Println(data)
+
+	data1 := []*struct{ num int }{{1}, {2}, {3}}
+	for _, v := range data1 {
+		v.num *= 10 // 对于指针类似的值，直接更新，不用采用索引更新
+	}
+	fmt.Println(data1[0], data1[1], data1[2]) // &{10} &{20} &{30}
 }
