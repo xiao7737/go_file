71d3d89513361d7f82f3015c34cf343a30b61541
xiao7737 <773708486@qq.com>
2020-07-04 17:21:26 +0800

    二维数组，查找元素

---
+++ algo/sword_offer/04/04_test.go
@@ -0,0 +1,43 @@
+package _4
+
+import "testing"
+
+func TestFindNumberIn2DArray(t *testing.T) {
+	tests := []struct {
+		input  [][]int
+		target int
+		want   bool
+	}{
+		{[][]int{{1, 2, 3}, {3, 4, 5}}, 5, true},
+		{[][]int{{1, 2, 3}, {3, 4, 5}}, 6, false},
+		{[][]int{{5}}, 5, true},
+		{[][]int{{}}, 5, false},
+	}
+	t.Run("FindNumberIn2DArray1", func(t *testing.T) {
+		for _, test := range tests {
+			if got := FindNumberIn2DArray1(test.input, test.target); got != test.want {
+				t.Errorf("got：%v, want:%v", got, test.want)
+			}
+		}
+	})
+	t.Run("FindNumberIn2DArray2", func(t *testing.T) {
+		for _, test := range tests {
+			if got := FindNumberIn2DArray1(test.input, test.target); got != test.want {
+				t.Errorf("got：%v, want:%v", got, test.want)
+			}
+		}
+	})
+}
+
+func BenchmarkFindRepeatNum(b *testing.B) {
+	b.Run("FindRepeatNum1", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			FindNumberIn2DArray1([][]int{{1, 2, 3}, {3, 4, 5}}, 5)
+		}
+	})
+	b.Run("FindRepeatNum2", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			FindNumberIn2DArray2([][]int{{1, 2, 3}, {3, 4, 5}}, 5)
+		}
+	})
+}
---
+++ algo/sword_offer/04/find_num_in_2d_arr.go
@@ -0,0 +1,45 @@
+package _4
+
+//判断一个数是否在一个二维数组中
+//该二维数组每行递增，每列递增
+
+//暴力法，直接遍历整个二维数组
+//时间O(M*N)，空间O(1)
+func FindNumberIn2DArray1(matrix [][]int, target int) bool {
+	if len(matrix) == 0 || len(matrix[0]) == 0 {
+		return false
+	}
+	rows, cols := len(matrix), len(matrix[0])
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			if matrix[i][j] == target {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+//减枝，利用行和列都是递增的条件
+//利用最右上角的数字进行对比
+//如果比target大，则该列的数都比target大，则去除这一列
+//如果比target小，则该行的数都比target小，则去除这一行，target肯定不在这一行
+//整体思想类似二分查找和二叉搜索树
+//时间O(M+N)，空间O(1)
+func FindNumberIn2DArray2(matrix [][]int, target int) bool {
+	if len(matrix) == 0 || len(matrix[0]) == 0 {
+		return false
+	}
+	rows, cols := len(matrix), len(matrix[0])
+	row, col := 0, cols-1
+	for row < rows && col >= 0 {
+		if matrix[row][col] == target {
+			return true
+		} else if matrix[row][col] > target {
+			col--
+		} else {
+			row++
+		}
+	}
+	return false
+}
