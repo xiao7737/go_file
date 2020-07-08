762e4033c7e11b0128f9bc557121c2244ea8a2e1
xiao7737 <773708486@qq.com>
2020-07-08 20:39:14 +0800

    翻转链表

---
+++ algo/sword_offer/24_reverse_list/reverse_list.go
@@ -0,0 +1,18 @@
+package _4_reverse_list
+
+//翻转链表
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+//双指针迭代
+func reverseList(head *ListNode) *ListNode {
+
+}
+
+//递归
+func reverseList1(head *ListNode) *ListNode {
+
+}
