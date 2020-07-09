120c1f05eb5d4d7561bdc8bcafd500e2b1dfbeaa
xiao7737 <773708486@qq.com>
2020-07-09 14:15:02 +0800

    24 reverselist

--- algo/sword_offer/24_reverse_list/reverse_list.go
+++ algo/sword_offer/24_reverse_list/reverse_list.go
@@ -7,12 +7,28 @@ type ListNode struct {
 	Next *ListNode
 }
 
-//双指针迭代
+//双指针迭代   时间O(N), 空间O(1)
 func reverseList(head *ListNode) *ListNode {
-
+	cur := head
+	var pre *ListNode
+	for cur != nil {
+		temp := cur.Next
+		cur.Next = pre
+		//往后迭代
+		pre = cur
+		cur = temp
+	}
+	return pre
 }
 
-//递归
+//递归 	时间O(N), 空间O(N)
 func reverseList1(head *ListNode) *ListNode {
-
+	if head == nil || head.Next == nil {
+		return head
+	}
+	p := reverseList1(head.Next)
+	head.Next.Next = head
+	//终止本次递归，防止链表循环
+	head.Next = nil
+	return p
 }
