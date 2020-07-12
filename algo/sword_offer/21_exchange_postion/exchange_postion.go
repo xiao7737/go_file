655302a4ae41a003d9ed2f746e3c737dc129e34c
xiao7737 <773708486@qq.com>
2020-07-12 23:16:14 +0800

    21 奇数在前的另一种写法

--- algo/sword_offer/21_exchange_postion/exchange_postion.go
+++ algo/sword_offer/21_exchange_postion/exchange_postion.go
@@ -8,14 +8,14 @@ func exchange(nums []int) []int {
 	i, j := 0, len(nums)-1
 	for i < j {
 		for nums[i]%2 == 1 {
-			//全是奇数
+			//从前到后全是奇数
 			if i == j {
 				return nums
 			}
 			i++
 		}
 		for nums[j]%2 == 0 {
-			//全是偶数
+			//从后到前全是偶数
 			if j == i {
 				return nums
 			}
@@ -24,6 +24,26 @@ func exchange(nums []int) []int {
 		nums[i], nums[j] = nums[j], nums[i]
 		i++
 		j--
+	}
+	return nums
+}
+
+// 双指针的另一种写法
+func exchange1(nums []int) []int {
+	i, j := 0, len(nums)-1
+	for i < j {
+		//找到奇数和偶数，交换
+		if nums[i]%2 == 0 && nums[j]%2 != 0 {
+			nums[i], nums[j] = nums[j], nums[i]
+		}
+		//从前往后遍历，奇数就i++
+		for i < len(nums) && nums[i]%2 != 0 {
+			i++
+		}
+		//从后往前遍历，偶数就j--
+		for j >= 0 && nums[j]%2 == 0 {
+			j--
+		}
 	}
 	return nums
 }
