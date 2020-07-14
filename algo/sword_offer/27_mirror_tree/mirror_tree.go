4f860277c1b0c71ad23f9c50be0c5d10c2ba991f
xiao7737 <773708486@qq.com>
2020-07-14 22:50:37 +0800

    27树的镜像

---
+++ algo/sword_offer/27_mirror_tree/mirror_tree.go
@@ -0,0 +1,21 @@
+package _7_mirror_tree
+
+//求树的镜像树，即左右节点完全交换
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+//递归，dfs，直接将左右节点进行交换即可
+//时间，空间都是O(N)
+func mirrorTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left, root.Right = mirrorTree(root.Right), mirrorTree(root.Left)
+	return root
+}
+
+//利用栈
+//时间，空间都是O(N)
