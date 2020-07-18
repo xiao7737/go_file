8c2d2ab368123d65f3c41385ac3de330cf55e1f3
xiao7737 <773708486@qq.com>
2020-07-18 19:38:32 +0800

    39 超过一半的数字

---
+++ algo/sword_offer/39_more_half_num/half_num.go
@@ -0,0 +1,31 @@
+package _9_more_half_num
+
+import "sort"
+
+//数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字
+
+//方案1：排序后，中间的那个数即是答案
+//时间O(NlogN)
+func majorityElement(nums []int) int {
+	sort.Ints(nums)
+	return nums[len(nums)>>1]
+}
+
+//方案2：摩尔投票，对每一个数进行投票，如果该数和下一个数不一样，将投票数-1
+//时间O(N)，只需要遍历一次
+func majorityElement1(nums []int) int {
+	x, votes := nums[0], 1
+	for i := 1; i < len(nums); i++ {
+		//票数归零，开始新一轮计数
+		if votes == 0 {
+			x = nums[i]
+		}
+
+		if nums[i] == x {
+			votes++
+		} else {
+			votes--
+		}
+	}
+	return x
+}
