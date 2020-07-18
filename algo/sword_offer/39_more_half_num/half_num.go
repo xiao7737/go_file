fe0045d7624d5773c6043190b1c4619f20200730
xiao7737 <773708486@qq.com>
2020-07-18 19:42:09 +0800

    复杂度解释

--- algo/sword_offer/39_more_half_num/half_num.go
+++ algo/sword_offer/39_more_half_num/half_num.go
@@ -5,7 +5,8 @@ import "sort"
 //数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字
 
 //方案1：排序后，中间的那个数即是答案
-//时间O(NlogN)
+//时间O(N*logN)
+//空间O(logN)
 func majorityElement(nums []int) int {
 	sort.Ints(nums)
 	return nums[len(nums)>>1]
@@ -13,6 +14,7 @@ func majorityElement(nums []int) int {
 
 //方案2：摩尔投票，对每一个数进行投票，如果该数和下一个数不一样，将投票数-1
 //时间O(N)，只需要遍历一次
+//空间O(1)
 func majorityElement1(nums []int) int {
 	x, votes := nums[0], 1
 	for i := 1; i < len(nums); i++ {
