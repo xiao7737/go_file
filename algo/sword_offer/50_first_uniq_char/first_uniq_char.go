7acfddc8bff67f9db9b88d0935c475d27cacdaad
xiao7737 <773708486@qq.com>
2020-07-25 20:19:25 +0800

    50利用map和slice返回 ：第一次出现一次的元素

--- algo/sword_offer/50_first_uniq_char/first_uniq_char.go
+++ algo/sword_offer/50_first_uniq_char/first_uniq_char.go
@@ -2,9 +2,33 @@ package _0_first_uniq_char
 
 //在字符串 s 中找出第一个只出现一次的字符，如果没有，返回一个单空格。 s只包含小写字母
 
+//利用hash表，key是元素，value是元素出现的次数
+//需要扫描两次，扫描第一次，进行计数，第二次，返回次数为1的元素，总的时间O(N)
+//注意go的map遍历返回时无序的，用slice保证顺序
 func firstUniqChar(s string) byte {
 	l := len(s)
 	if l == 0 {
 		return byte(' ')
 	}
+	bytes := []byte(s)
+	m := make(map[byte]int, len(s))
+	sli := make([]byte, 0)
+
+	for _, v := range bytes {
+		if _, ok := m[v]; ok {
+			//map中已经存在该元素
+			m[v] += 1
+		} else {
+			//map中不存在该元素
+			m[v] = 1
+			sli = append(sli, v)
+		}
+	}
+
+	for _, v := range sli {
+		if m[v] == 1 {
+			return v
+		}
+	}
+	return byte(' ')
 }
