8d08d53fcbcd6b6333ded1727a40adb2d72b73f2
xiao7737 <773708486@qq.com>
2020-07-25 16:54:10 +0800

    40返回最小的k个数

--- algo/sword_offer/40_min_kth_num/kth_min_num.go
+++ algo/sword_offer/40_min_kth_num/kth_min_num.go
@@ -18,12 +18,17 @@ func getLeastNumbers(arr []int, k int) []int {
 	return res
 }
 
-//利用堆
+//利用堆或者红黑树，时间O(N*logK)
+//堆不满k时，就添加进堆，满K时，如果添加元素比堆的最大元素小，则入堆，否则则丢弃
 func getLeastNumbers1(arr []int, k int) []int {
-
 }
 
 //利用快排思想
 func getLeastNumbers2(arr []int, k int) []int {
+	//会修改原数组
+}
 
+//利用计数排序
+func getLeastNumbers3(arr []int, k int) []int {
+	//会修改原数组
 }
