0f5bbcfceb77f2469e9b56bd3a9e82bfff59d7fe
xiao7737 <773708486@qq.com>
2020-08-19 20:27:50 +0800

    58-2 坐旋转字符串

---
+++ algo/sword_offer/58_2_reverseLeftWord/reverse_left_word.go
@@ -0,0 +1,29 @@
+package _8_2_reverseLeftWord
+
+//输入字符串s 和 n，将字符串前k个元素移动到字符串尾部
+// 输入abcdef，2，  输出为cdefab
+
+//方案1 利用字符串是可读的byte切片特性，速度快
+func reverseLeftWord(s string, n int) string {
+	if len(s) <= n {
+		return s
+	}
+	return s[n:] + s[:n]
+}
+
+//方案2 多次string相加导致效率不佳
+func reverseLeftWord2(s string, n int) string {
+	if len(s) <= n {
+		return s
+	}
+	var str1 string
+	var str2 string
+	for i := 0; i < len(s); i++ {
+		if i < n {
+			str1 += string(s[i])
+		} else {
+			str2 += string(s[i])
+		}
+	}
+	return str2 + str1
+}
