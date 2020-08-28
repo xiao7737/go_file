85245d877f57de68ae80aeaff6832676bf2c4b57
xiao7737 <773708486@qq.com>
2020-08-28 23:29:49 +0800

    rename file

--- main.go
+++ main.go
@@ -5,17 +5,13 @@ import (
 )
 
 func main() {
-	str1 := "ee"
-	strArr := []string{"aa", "bb", "cc", "dd"}
-	exists := inArray(str1, strArr)
-	fmt.Println(exists)
+	fmt.Println(test(true))
 }
 
-func inArray(need string, needArr []string) bool {
-	for _, v := range needArr {
-		if need == v {
-			return true
-		}
+func test(b bool) (a int) {
+	if b {
+		a := 2
+		a += 1
 	}
-	return false
+	return a
 }
